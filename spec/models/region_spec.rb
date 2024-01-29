1f191ebe5c924729ba83f231f18d6dcf55caa2df
Xander Bailey <xander.bailey@gmail.com>
2024-01-29 15:04:16 -0800

    Refactor region_spec.rb

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,18 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
-  it "exists" do
-    Region.new
-  end
+  let (:region) { Region.new }
 
   it "has a name" do
-    region = Region.new
     expect(region).to respond_to(:name)
   end
 
   it "has a string representation that is its name" do
     name = 'Mt. Hood'
-    region = Region.new(name: name)
     result = region.to_s
   end
 
