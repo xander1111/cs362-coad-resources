421b21ff7379cfed18b7705edca8583c60138931
Xander Bailey <xander.bailey@gmail.com>
2024-01-29 18:12:29 -0800

    Fix user email uniqueness test not using case_insensitive

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe User, type: :model do
   it { should validate_length_of(:email).is_at_least(1).is_at_most(255) }
   it { should allow_value("user@example.com").for(:email) }
   it { should_not allow_value("not an email").for(:email) }
-  it { should validate_uniqueness_of(:email) }
+  it { should validate_uniqueness_of(:email).case_insensitive }
   it { should validate_presence_of(:password) }
   it { should validate_length_of(:password).is_at_least(7).is_at_most(255) }
 
