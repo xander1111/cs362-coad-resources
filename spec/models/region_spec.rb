513cdf9e140b311740864f6738875769e938dd4f
Xander Bailey <xander.bailey@gmail.com>
2024-01-29 18:13:30 -0800

    Add validation checks to region_spec.rb

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -16,4 +16,8 @@ RSpec.describe Region, type: :model do
 
   it { should have_many(:tickets) }
 
+  it { should validate_presence_of(:name) }
+  it { should validate_length_of(:name).is_at_least(1).is_at_most(255) }
+  it { should validate_uniqueness_of(:name).case_insensitive }
+
 end
