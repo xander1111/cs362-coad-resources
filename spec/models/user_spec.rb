4d6a72c68c10ecd544bac14d22212873aed1b9bb
Xander Bailey <xander.bailey@gmail.com>
2024-01-29 18:01:08 -0800

    Add validation checks to user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,12 @@ RSpec.describe User, type: :model do
 
   it { should belong_to(:organization).optional }
 
+  it { should validate_presence_of(:email) }
+  it { should validate_length_of(:email).is_at_least(1).is_at_most(255) }
+  it { should allow_value("user@example.com").for(:email) }
+  it { should_not allow_value("not an email").for(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_presence_of(:password) }
+  it { should validate_length_of(:password).is_at_least(7).is_at_most(255) }
+
 end
