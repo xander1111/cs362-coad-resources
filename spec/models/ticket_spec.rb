d10e05bd4a3bad8e43a8196b0161468033818617
Xander Bailey <xander.bailey@gmail.com>
2024-02-07 16:03:23 -0800

    Add get open tickets spec to ticket_spec.rb

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -74,4 +74,12 @@ RSpec.describe Ticket, type: :model do
     expect(ticket.to_s).to eq("Ticket 1")
   end
 
+  it "can provide a list of all open tickets" do
+    @reg = Region.create(:name => "test region")
+    @res_cat = ResourceCategory.create(:name => "test category")
+    @open_ticket = Ticket.create(:closed => false, :region => @reg, :resource_category => @res_cat, :name => "open", :phone => "+1 555 555 5555")
+    @closed_ticket = Ticket.create(:closed => true, :region => @reg, :resource_category => @res_cat, :name => "closed", :phone => "+1 555 555 5555") 
+    expect(Ticket.open).to contain_exactly(@open_ticket)
+  end
+
 end
