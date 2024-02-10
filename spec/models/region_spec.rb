6d8959d274688c87c77084b337ec67f9da31176b
Xander Bailey <xander.bailey@gmail.com>
2024-02-09 20:18:53 -0800

    Add unspecified region spec to region_spec.rb

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -20,4 +20,8 @@ RSpec.describe Region, type: :model do
   it { should validate_length_of(:name).is_at_least(1).is_at_most(255) }
   it { should validate_uniqueness_of(:name).case_insensitive }
 
+  it "can provide a 'unspecified' region" do
+    expect(Region.unspecified.name).to eq("Unspecified")
+  end
+
 end
