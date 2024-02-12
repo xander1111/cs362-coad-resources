b21ca51ae452545566218eda0a0e3b5459f5dec9
Xander Bailey <xander.bailey@gmail.com>
2024-02-12 14:20:43 -0800

    Skip user email confirmation in factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,6 +3,9 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "example_user-#{n}@example.com" }
     password { "User_password" }
+
+    before(:create) { |user| user.skip_confirmation! }
+
   end
 
 end
