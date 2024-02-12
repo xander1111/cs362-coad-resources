2e63922e0e0c0a0ec738dbb660a75abaff7309ce
Xander Bailey <xander.bailey@gmail.com>
2024-02-12 13:54:03 -0800

    Add organization factory

---
+++ spec/factories/organization.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+
+  factory :organization do
+    sequence(:email) { |n| "example_org-#{n}@example.com" }
+    sequence(:name) { |n| "org ##{n}" }
+    sequence(:phone) { |n| "+1 555 555 #{1000 + n}" }
+    sequence(:secondary_phone) { |n| "+1 555 555 #{2000 + n}" }
+
+    status { nil }
+
+    sequence(:primary_name) { |n| "primary #{n}" }
+    sequence(:secondary_name) { |n| "secondary #{n}" }
+  end
+
+end
