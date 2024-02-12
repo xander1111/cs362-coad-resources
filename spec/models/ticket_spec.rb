48b0e429d37eb6809b495dc1fa57c66bde9a86ed
Xander Bailey <xander.bailey@gmail.com>
2024-02-12 14:46:16 -0800

    Refactor ticket_spec.rb to use factories for other models

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
-  let (:ticket) { FactoryBot.build_stubbed(:ticket) }
+  let (:ticket) { build_stubbed(:ticket) }
 
   it "has a name" do
     expect(ticket).to respond_to(:name)
@@ -65,7 +65,7 @@ RSpec.describe Ticket, type: :model do
   end
 
   it "can tell if there is a responsible organization" do
-    ticket.organization = FactoryBot.build_stubbed(:organization)
+    ticket.organization = build_stubbed(:organization)
     expect(ticket).to be_captured
   end
 
@@ -76,45 +76,43 @@ RSpec.describe Ticket, type: :model do
 
   describe "database tests" do
 
-    let (:reg1) { Region.create(:name => "test region 1") }
-    let (:reg2) { Region.create(:name => "test region 2") }
-    let (:resource_cat1) { ResourceCategory.create!(:name => "test category 1") }
-    let (:resource_cat2) { ResourceCategory.create!(:name => "test category 2") }
-    let (:org) { Organization.create!(:name => "test org", :email => "org@example.com", :phone => "+1 555 555 5556", :primary_name => "org pname", :secondary_name => "org sname", :secondary_phone => "+1 555 555 5557") }
+    let (:reg1) { build_stubbed(:region) }
+    let (:reg2) { build_stubbed(:region) }
+    let (:resource_cat1) { build_stubbed(:resource_category) }
+    let (:resource_cat2) { build_stubbed(:resource_category) }
+    let (:org) { build_stubbed(:organization) }
+    let (:open_ticket) { create(:ticket, closed: false, region: reg1, resource_category: resource_cat1) }
+    let (:closed_ticket) { create(:ticket, closed: true, region: reg1, resource_category: resource_cat2) }
+    let (:open_ticket_with_org) { create(:ticket, closed: false, region: reg2, resource_category: resource_cat1, organization: org) }
+    let (:closed_ticket_with_org) { create(:ticket, closed: true, region: reg2, resource_category: resource_cat2, organization: org) }
 
-    before do
-      @open_ticket = Ticket.create(:closed => false, :region => reg1, :resource_category => resource_cat1, :name => "open", :phone => "+1 555 555 5555")
-      @closed_ticket = Ticket.create(:closed => true, :region => reg1, :resource_category => resource_cat2, :name => "closed", :phone => "+1 555 555 5555")
-      @open_ticket_with_org = Ticket.create!(:closed => false, :region => reg2, :resource_category => resource_cat1, :name => "open", :phone => "+1 555 555 5555", :organization => org)
-      @closed_ticket_with_org = Ticket.create!(:closed => true, :region => reg2, :resource_category => resource_cat2, :name => "closed", :phone => "+1 555 555 5555", :organization => org)
-    end
 
     it "can provide a list of all open tickets" do
-      expect(Ticket.open).to contain_exactly(@open_ticket)
+      expect(Ticket.open).to contain_exactly(open_ticket)
     end
   
     it "can provide a list of all closed tickets" do
-      expect(Ticket.closed).to contain_exactly(@closed_ticket, @closed_ticket_with_org)
+      expect(Ticket.closed).to contain_exactly(closed_ticket, closed_ticket_with_org)
     end
 
     it "can provide a list of all open tickets with an assigned organization" do
-      expect(Ticket.all_organization).to contain_exactly(@open_ticket_with_org)
+      expect(Ticket.all_organization).to contain_exactly(open_ticket_with_org)
     end
 
     it "can find all open tickets with a specified organization" do
-      expect(Ticket.organization(org.id)).to contain_exactly(@open_ticket_with_org)
+      expect(Ticket.organization(org.id)).to contain_exactly(open_ticket_with_org)
     end
 
     it "can find all closed tickets with a specified organization" do
-      expect(Ticket.closed_organization(org.id)).to contain_exactly(@closed_ticket_with_org)
+      expect(Ticket.closed_organization(org.id)).to contain_exactly(closed_ticket_with_org)
     end
 
     it "can find all tickets with a specified region" do
-      expect(Ticket.region(reg1.id)).to contain_exactly(@open_ticket, @closed_ticket)
+      expect(Ticket.region(reg1.id)).to contain_exactly(open_ticket, closed_ticket)
     end
 
     it "can find all tickets with a specified resource category" do
-      expect(Ticket.resource_category(resource_cat1.id)).to contain_exactly(@open_ticket, @open_ticket_with_org)
+      expect(Ticket.resource_category(resource_cat1.id)).to contain_exactly(open_ticket, open_ticket_with_org)
     end
 
   end
