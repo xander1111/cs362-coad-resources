e0754f7141ca7ccf052192ad96552e049414ba8b
Xander Bailey <xander.bailey@gmail.com>
2024-02-18 22:54:38 -0800

    Add name attribute spec to resource_category_spec.rb

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
+  it "has a name" do
+    @res_cat = build_stubbed(:resource_category)
+    expect(@res_cat).to respond_to(:name)
+  end
+
 end
