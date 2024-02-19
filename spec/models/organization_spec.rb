70ac6bef248a1ce8c5938bc25c311233590c987f
Xander Bailey <xander.bailey@gmail.com>
2024-02-18 22:30:32 -0800

    Add email format validator spec to organization_spec.rb

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -78,4 +78,7 @@ RSpec.describe Organization, type: :model do
   it { should validate_uniqueness_of(:email).case_insensitive }
   it { should validate_uniqueness_of(:name).case_insensitive }
 
+  it { should allow_value("user@example.com").for(:email) }
+  it { should_not allow_value("not an email").for(:email) }
+
 end
