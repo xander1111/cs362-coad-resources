13a1f39bf5e4594ae1641389311842f171fcab06
Xander Bailey <xander.bailey@gmail.com>
2024-02-18 22:17:23 -0800

    Add length validator specs to organization_spec.rb

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -71,4 +71,8 @@ RSpec.describe Organization, type: :model do
   it { should validate_presence_of(:secondary_name) }
   it { should validate_presence_of(:secondary_phone) }
 
+  it { should validate_length_of(:email).is_at_least(1).is_at_most(255) }
+  it { should validate_length_of(:name).is_at_least(1).is_at_most(255) }
+  it { should validate_length_of(:description).is_at_most(1020) }
+
 end
