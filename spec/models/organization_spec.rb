049f8cf21ab814a92b68b22db84ff23b98736e07
Xander Bailey <xander.bailey@gmail.com>
2024-02-18 21:05:54 -0800

    Add name attribute spec to organization_spec.rb

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
+  it "has a name" do
+      org = build_stubbed(:organization)
+    expect(org).to respond_to(:name)
+  end
+
 end
