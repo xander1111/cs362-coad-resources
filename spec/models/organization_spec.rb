01a4211e7fda4db50550457c2050ffab5c7841f8
Xander Bailey <xander.bailey@gmail.com>
2024-02-18 21:15:28 -0800

    Add status attribute spec to organization_spec.rb

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
+  let (:org) { build_stubbed(:organization) }
+
   it "has a name" do
-      org = build_stubbed(:organization)
     expect(org).to respond_to(:name)
+  end
+
+  it "has a status" do
+    expect(org).to respond_to(:status)
   end
 
 end
