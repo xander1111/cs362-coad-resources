cfb0073a99a1b58e6d040c7fd69a3aecaeb9e18f
Alexander <146288671+alexb-osucascades@users.noreply.github.com>
2024-02-26 15:19:35 -0800

    commit regionControllerSpec

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -2,4 +2,27 @@ require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
 
+  context 'as an admin user' do
+    let(:user) { create(:user, :admin) }
+    before(:each) { sign_in(user) }
+
+    let(:region) { create(:region) }
+
+    describe "POST #create" do
+      it {
+        post(:create, params: { region: attributes_for(:region) })
+        expect(response).to redirect_to(regions_path)
+      }
+
+      it {
+        expect_any_instance_of(Region).to receive(:save).and_return(false)
+        post(:create, params: { region: attributes_for(:region) })
+        expect(response).to be_successful
+      }
+
+
+    end
+
+  end
+
 end
