8c3469dd89693ac46db271933f733cb02a88eb2a
Xander Bailey <xander.bailey@gmail.com>
2024-02-26 14:45:18 -0800

    Add GET /organizations spec

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -2,4 +2,39 @@ require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
 
+  describe "GET /organizations" do
+
+    context "logged out user" do
+    
+      it "redirects to sign in page" do
+        get :index
+        expect(response).to redirect_to('/users/sign_in')
+      end
+
+    end
+
+    context "normal user" do
+        
+      it "should be successful" do
+        user = create(:user)
+        sign_in(user) 
+        get :index
+        expect(response).to be_successful
+      end
+
+    end
+  
+    context "admin user" do
+  
+      it "should be successful" do
+        user = create(:user, role: :admin)
+        sign_in(user) 
+        get :index
+        expect(response).to be_successful
+      end
+
+    end
+
+  end
+
 end
