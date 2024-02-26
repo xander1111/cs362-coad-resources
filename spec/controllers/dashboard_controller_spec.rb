9cfdc7b66401ffa42bd83c637a66baf3b17bd63b
Xander Bailey <xander.bailey@gmail.com>
2024-02-26 14:32:16 -0800

    Add GET /dashboard spec

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,4 +2,40 @@ require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
 
+  describe "GET /dashboard" do
+
+    context "logged out user" do
+
+      let (:user) { nil }
+  
+      it "redirects to sign in page" do
+        get :index
+        expect(response).to redirect_to('/users/sign_in')
+      end
+  
+    end
+  
+    context "normal user" do
+        
+      it "should be successful" do
+        user = create(:user)
+        sign_in(user) 
+        get :index
+        expect(response).to be_successful
+      end
+
+    end
+  
+    context "admin user" do
+  
+      it "should be successful" do
+        user = create(:user, role: :admin)
+        sign_in(user) 
+        get :index
+        expect(response).to be_successful
+      end
+
+    end
+    
+  end
 end
