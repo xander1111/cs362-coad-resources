33cc6ac156a479f0bbd3bae2d0adf9a793a551c9
Xander Bailey <xander.bailey@gmail.com>
2024-03-03 13:54:06 -0800

    Add approved user context for organization controller specs

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -44,7 +44,7 @@ RSpec.describe OrganizationsController, type: :controller do
 
   end
 
-  context "normal user" do
+  context "unapproved user" do
 
     let (:user) { create(:user) }
     before(:each) {
@@ -91,6 +91,56 @@ RSpec.describe OrganizationsController, type: :controller do
         expect(response).to redirect_to(dashboard_path)
       end
     end
+
+  end
+
+
+  context "approved user" do
+
+    let (:approved_org) { create(:organization, status: :approved) }
+    let (:user) { create(:user, organization: approved_org) }
+    before(:each) {
+      create(:user, role: :admin)
+      sign_in(user)
+    }
+
+    describe "GET /organizations" do
+      it "should be successful" do
+        get :index
+        expect(response).to be_successful
+      end
+
+    end
+
+    describe "POST /organizations" do
+      it "redirects to the dashboard" do
+        post(:create, params: { organization: attributes_for(:organization) })
+        expect(response).to redirect_to(dashboard_path)
+      end
+    end
+
+    describe "GET /organizations/new" do
+      it "redirects to dashboard" do
+        get :new
+        expect(response).to redirect_to(dashboard_path)
+      end
+    end
+
+    describe "GET /organizations/:id" do
+      it "should be successful" do
+        get :show, params: { id: 1 }
+        expect(response).to be_successful
+      end
+    end
+
+    describe "GET /organizations/:id/edit" do
+      it "should be successful" do
+        allow(user).to receive(:organization).and_return(double(:organization, approved?: true))
+        get :edit, params: { id: 1 }
+        expect(response).to be_successful
+      end
+    end
+
   end
 
   context "admin user" do
