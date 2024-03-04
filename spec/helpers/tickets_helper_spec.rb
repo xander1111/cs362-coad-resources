2cc2803a783ed0ff29de9d205cb550154ef015b5
Xander Bailey <xander.bailey@gmail.com>
2024-03-03 23:19:50 -0800

    Add format_phone_number() spec

--- spec/helpers/tickets_helper_spec.rb
+++ spec/helpers/tickets_helper_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the TIcketsHelper. For example:
-#
-# describe TIcketsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe TicketsHelper, type: :helper do
+
+  describe "format_phone_number" do
+
+    it "formats a phone number using a US country code" do
+      expect(helper.format_phone_number("555 555 1234")).to eq("+15555551234")
+    end
+
+  end
 
 end
