64f36e8ed6e17e7e063840962967350306c60f58
Xander Bailey <xander.bailey@gmail.com>
2024-03-03 21:57:21 -0800

    Add static_pages#index specs

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,4 +2,44 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
 
+  context "no user" do
+
+    describe "static_pages#index" do
+      it "should be successful" do
+        get :index
+        expect(response).to be_successful
+      end
+    end
+
+  end
+
+  context "normal user" do
+
+    let (:user) { create(:user) }
+    before(:each) { sign_in(user) }
+
+    describe "static_pages#index" do
+      it "should be successful" do
+        get :index
+        expect(response).to be_successful
+      end
+    end
+
+  end
+
+
+  context "admin user" do
+
+    let (:user) { create(:user, role: :admin) }
+    before(:each) { sign_in(user) }
+
+    describe "static_pages#index" do
+      it "should be successful" do
+        get :index
+        expect(response).to be_successful
+      end
+    end
+
+  end
+
 end
