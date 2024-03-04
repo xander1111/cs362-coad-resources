9318e9ee83042effd28c656b3ac5e11b5bf64544
Xander Bailey <xander.bailey@gmail.com>
2024-03-03 23:10:56 -0800

    Add full_title() specs

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
 
+  describe "full_title" do
+  
+    it "returns the website name when not passed a string" do
+      expect(helper.full_title).to eq("Disaster Resource Network")
+    end
+
+    it "returns the string given with the website name appended" do
+      @page_name = "test page name"
+      expect(helper.full_title(@page_name)).to eq(@page_name << " | Disaster Resource Network")
+    end
+
+  end
+
 end
