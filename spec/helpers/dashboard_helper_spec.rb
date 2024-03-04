d6a8c384089f19d6fd6e06c7eaf935bdbcb0627e
Xander Bailey <xander.bailey@gmail.com>
2024-03-03 23:16:36 -0800

    Add dashboard_for() specs

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -12,4 +12,28 @@ require 'rails_helper'
 # end
 RSpec.describe DashboardHelper, type: :helper do
 
+  describe "dashboard_for" do
+
+    it "returns the admin dashboard for a admin user" do
+      @user = build_stubbed(:user, role: :admin)
+      expect(helper.dashboard_for(@user)).to eq("admin_dashboard")
+    end
+
+    it "returns the organization submitted dashboard for a user in a submitted organization" do
+      @user = build_stubbed(:user, organization: build_stubbed(:organization, status: :submitted))
+      expect(helper.dashboard_for(@user)).to eq("organization_submitted_dashboard")
+    end
+
+    it "returns the organization approved dashboard for a user in an approved organization" do
+      @user = build_stubbed(:user, organization: build_stubbed(:organization, status: :approved))
+      expect(helper.dashboard_for(@user)).to eq("organization_approved_dashboard")
+    end
+
+    it "returns the create application dashboard for a user with no organization" do
+      @user = build_stubbed(:user)
+      expect(helper.dashboard_for(@user)).to eq("create_application_dashboard")
+    end
+
+  end
+
 end
