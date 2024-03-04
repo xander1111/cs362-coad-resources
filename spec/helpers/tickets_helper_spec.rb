b4da1dd88ed8cb5203cb0acae776e674d031d191
Xander Bailey <xander.bailey@gmail.com>
2024-03-03 23:21:41 -0800

    Change input phone number format for format_phone_number spec

--- spec/helpers/tickets_helper_spec.rb
+++ spec/helpers/tickets_helper_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe TicketsHelper, type: :helper do
   describe "format_phone_number" do
 
     it "formats a phone number using a US country code" do
-      expect(helper.format_phone_number("555 555 1234")).to eq("+15555551234")
+      expect(helper.format_phone_number("(555) 555-1234")).to eq("+15555551234")
     end
 
   end
