76bd651d9b567355cb707cf39d18a0fca6bcb0c8
Xander Bailey <xander.bailey@gmail.com>
2024-03-03 22:12:36 -0800

    Add resource_category#index specs

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -2,4 +2,44 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
 
+  context "no user" do
+
+    describe "resource_category#index" do
+      it "redirects to sign in page" do
+        get :index
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+  end
+
+  context "normal user" do
+
+    let (:user) { create(:user) }
+    before(:each) { sign_in(user) }
+
+    describe "resource_category#index" do
+      it "redirects to dashboard" do
+        get :index
+        expect(response).to redirect_to(dashboard_path)
+      end
+    end
+
+  end
+
+
+  context "admin user" do
+
+    let (:user) { create(:user, role: :admin) }
+    before(:each) { sign_in(user) }
+
+    describe "resource_category#index" do
+      it "should be successful" do
+        get :index
+        expect(response).to be_successful
+      end
+    end
+
+  end
+
 end
