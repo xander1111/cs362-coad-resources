68a4728f652fdf9f41b68a13dd429918707d910a
Xander Bailey <xander.bailey@gmail.com>
2024-03-03 22:30:35 -0800

    Add resource_category#edit specs

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -34,6 +34,13 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       end
     end
 
+    describe "resource_category#edit" do
+      it "redirects to sign in page" do
+        get :edit, params: { id: res_cat.id }
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
   end
 
   context "normal user" do
@@ -65,6 +72,13 @@ RSpec.describe ResourceCategoriesController, type: :controller do
     describe "resource_category#create" do
       it "redirects to dashboard" do
         post :create, params: { resource_category: attributes_for(:resource_category) }
+        expect(response).to redirect_to(dashboard_path)
+      end
+    end
+
+    describe "resource_category#edit" do
+      it "redirects to dashboard" do
+        get :edit, params: { id: res_cat.id }
         expect(response).to redirect_to(dashboard_path)
       end
     end
@@ -102,6 +116,13 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       it "redirects to the resource categories index page" do
         post :create, params: { resource_category: attributes_for(:resource_category) }
         expect(response).to redirect_to(resource_categories_path)
+      end
+    end
+
+    describe "resource_category#edit" do
+      it "should be successful" do
+        get :edit, params: { id: res_cat.id }
+        expect(response).to be_successful
       end
     end
 
