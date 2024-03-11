278beb17ca28446053a8c4ce720ff8a5caed01fb
Xander Bailey <xander.bailey@gmail.com>
2024-03-11 13:27:07 -0700

    Refactor resource_categories_controller_spec.rb

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -64,6 +64,8 @@ RSpec.describe ResourceCategoriesController, type: :controller do
 
     describe "resource_categories#destroy" do
       it "redirects to sign in page" do
+        allow(DeleteResourceCategoryService).to receive(:new).and_return(double(run!: nil)) 
+        
         delete :destroy, params: { id: res_cat.id }
         expect(response).to redirect_to(new_user_session_path)
       end
@@ -134,6 +136,8 @@ RSpec.describe ResourceCategoriesController, type: :controller do
 
     describe "resource_categories#destroy" do
       it "redirects to dashboard" do
+        allow(DeleteResourceCategoryService).to receive(:new).and_return(double(run!: nil)) 
+        
         delete :destroy, params: { id: res_cat.id }
         expect(response).to redirect_to(dashboard_path)
       end
@@ -169,10 +173,19 @@ RSpec.describe ResourceCategoriesController, type: :controller do
     end
 
     describe "resource_categories#create" do
-      it "redirects to the resource categories index page" do
+
+      it "redirects to the resource categories index page on success" do
         post :create, params: { resource_category: attributes_for(:resource_category) }
         expect(response).to redirect_to(resource_categories_path)
       end
+
+      it "stays on the new page on failure" do
+        allow_any_instance_of(ResourceCategory).to receive(:save).and_return(false)
+
+        post :create, params: { resource_category: attributes_for(:resource_category) }
+        expect(response).to be_successful
+      end
+
     end
 
     describe "resource_categories#edit" do
@@ -183,28 +196,57 @@ RSpec.describe ResourceCategoriesController, type: :controller do
     end
 
     describe "resource_categories#update" do
-      it "redirects to the updated resource categories page" do
+
+      it "redirects to the updated resource categories page on success" do
         patch :update, params: { id: res_cat.id, resource_category: attributes_for(:resource_category) }
         expect(response).to redirect_to(resource_category_path(res_cat.id))
       end
+
+      it "stays on the edit page on failure" do
+        allow_any_instance_of(ResourceCategory).to receive(:save).and_return(false)
+
+        patch :update, params: { id: res_cat.id, resource_category: attributes_for(:resource_category) }
+        expect(response).to be_successful
+      end
+
     end
 
     describe "resource_categories#activate" do
-      it "redirects to the activated resource categories page" do
+
+      it "redirects to the activated resource categories page on success" do
         patch :activate, params: { id: res_cat.id }
         expect(response).to redirect_to(resource_category_path(res_cat.id))
       end
+
+      it "redirects to the failed to activate resource categories page on failure" do
+        allow_any_instance_of(ResourceCategory).to receive(:save).and_return(false)
+
+        patch :activate, params: { id: res_cat.id }
+        expect(response).to redirect_to(resource_category_path(res_cat.id))
+      end
+
     end
 
     describe "resource_categories#deactivate" do
-      it "redirects to the deactivated resource categories page" do
+
+      it "redirects to the deactivated resource categories page on success" do
         patch :deactivate, params: { id: res_cat.id }
         expect(response).to redirect_to(resource_category_path(res_cat.id))
       end
+      
+      it "redirects to the failed to deactivate resource categories page on failure" do
+        allow_any_instance_of(ResourceCategory).to receive(:save).and_return(false)
+        
+        patch :deactivate, params: { id: res_cat.id }
+        expect(response).to redirect_to(resource_category_path(res_cat.id))
+      end
+
     end
 
     describe "resource_categories#destroy" do
       it "redirects to the resource categories page" do
+        allow(DeleteResourceCategoryService).to receive(:new).and_return(double(run!: nil)) 
+
         delete :destroy, params: { id: res_cat.id }
         expect(response).to redirect_to(resource_categories_path)
       end
