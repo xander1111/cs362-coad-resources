3e1aee8ce0ebad9cbd6213d28eb75d9fae046ed1
Xander Bailey <xander.bailey@gmail.com>
2024-03-11 15:38:01 -0700

    Add region deletion e2e spec

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
 
+  before(:each) {
+    @user = create(:user, role: :admin)
+    log_in_as(@user)
+
+    @region_name = "Test Region for e2e"
+    @region = create(:region, name: @region_name)
+  }
+
+  it "can delete a new region from the home screen" do
+
+    visit root_path
+
+    click_on "Regions"
+    click_on @region_name
+
+    click_on "Delete"
+
+    expect(current_path).to eq regions_path
+
+  end
+
 end
