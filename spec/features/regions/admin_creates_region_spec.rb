837a25b5401276a431055b1a77437dbf89f4e7a0
Xander Bailey <xander.bailey@gmail.com>
2024-03-11 15:37:50 -0700

    Add region creation e2e spec

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
 
+  before(:each) {
+    @user = create(:user, role: :admin)
+    log_in_as(@user)
+  }
+
+  it "can create a new region from the home screen" do
+
+    visit root_path
+
+    click_on "Regions"
+    click_on "Add Region"
+
+    fill_in "Name", with: "Feature test region"
+    click_on "Add Region"
+
+    expect(current_path).to eq regions_path
+
+  end
+
 end
