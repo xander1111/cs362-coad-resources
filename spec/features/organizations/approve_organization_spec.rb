4c4c95220979bcf767b83651ae76f94dbbd0f73c
Xander Bailey <xander.bailey@gmail.com>
2024-03-13 10:20:10 -0700

    Add organization approval e2e spec

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -2,4 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
 
+  before(:each) {
+    @user = create(:user, role: :admin)
+    log_in_as(@user)
+
+    @org_name = "Test Org for approval"
+    @org = create(:organization, name: @org_name, status: :submitted)
+  }
+
+  it "can approve an organization from the home screen" do
+
+    visit root_path
+    
+    click_on "Organizations"
+    click_on "Pending"
+    click_on @org_name
+
+    click_on "Approve"
+
+    expect(current_path).to eq organizations_path
+
+    click_on "Approved"
+
+    expect(page).to have_selector("#organization_#{@org.id}")
+
+  end
+
 end
