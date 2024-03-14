977fc2eb34e3e3509a7c1a81ff215468015f1b74
Xander Bailey <xander.bailey@gmail.com>
2024-03-13 18:23:08 -0700

    Add user login e2e spec

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
 
+  before(:each) {
+    @user = create(:user)
+  }
+
+  it "can log in from the home page" do
+
+    visit root_path
+
+    click_on "Log in"
+
+    fill_in "Email address", with: @user.email
+    fill_in "Password", with: @user.password
+
+    click_on "Sign in"
+
+    expect(current_path).to eq(dashboard_path)
+
+  end
+
 end
