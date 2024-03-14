a21e3b0a182582a369507f6f0e198650c2cafcc1
Xander Bailey <xander.bailey@gmail.com>
2024-03-13 18:32:29 -0700

    Add user registration e2e spec

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
 
+  it "can create a new account from the home page" do
+
+    visit root_path
+
+    click_on "Sign up"
+
+    fill_in "Email address", with: "test_email@example.com"
+    fill_in "Password", with: "Test_Password"
+    fill_in "Password confirmation", with: "Test_Password"
+
+    click_on "commit"
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_text("confirmation link")
+
+  end
+
 end
