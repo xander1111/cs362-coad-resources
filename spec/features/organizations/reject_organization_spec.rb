1378852705e5b4271686e3739e4455af8899f73c
Xander Bailey <xander.bailey@gmail.com>
2024-03-13 17:21:30 -0700

    Add organization rejection e2e spec

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -2,4 +2,31 @@ require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
 
+  before(:each) {
+    @user = create(:user, role: :admin)
+    log_in_as(@user)
+
+    @org_name = "Test Org for approval"
+    @org = create(:organization, name: @org_name, status: :submitted)
+  }
+
+  it "can reject an organization from the home page" do
+
+    visit root_path
+
+    click_on "Organizations"
+    click_on "Pending"
+    click_on @org_name
+
+    fill_in "Rejection Reason", with: "Test reason"
+    click_on "Reject"
+
+    expect(current_path).to eq organizations_path
+
+    click_on "Rejected"
+
+    expect(page).to have_selector("#organization_#{@org.id}")
+
+  end
+
 end
